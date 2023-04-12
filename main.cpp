ce60e667e275e1fbe0e1500991e450143248fea4
Othmane Azzouzi <64237655+OthmaneAzzouzi@users.noreply.github.com>
2023-04-12 15:13:32 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,32 +6,42 @@
 
 using namespace std;
 
-int tp2(istream& in){
+int solution(std::istream& in){
     Tableau<Personne> personnes;
-
-    while(in){ 
+    while(in){
         Personne nouvellePersonne;
         in >> nouvellePersonne;
         if(in.eof()) break;
         personnes.ajouter(nouvellePersonne);
+
     }
 
+
     //  À Compléter : 2 boucles imbriquées pour déterminer et afficher le type de relation entre chaque personne.
-    for(int i = 0; i < personnes.taille(); i++)
+    for(int i = 0; i < personnes.taille(); i++) {
+
         for(int j = i + 1; j < personnes.taille(); j++){
-            string typeRelation = personnes[i].getTypeRelation(personnes[j]);
-            if(typeRelation != "")
-                cout << personnes[i].getNom() << " et " << personnes[j].getNom() << typeRelation << endl;
+            std::string typeRelation = personnes[i].getTypeRelation(personnes[j]);
+            if(typeRelation != "") {
+                std::cout << personnes[i].getNom() + " et " +  personnes[j].getNom() + " sont des " + typeRelation + "." << std::endl;
+            }
+        }
+
     }
+    // vider la memoire
+    for(int j = 0; j < personnes.taille(); ++j){
+        delete personnes[j].listeCoordonnees;
+    }
+
     return 0; // fin normale
 }
 
 int main(int argc, const char** argv)
 {
     if(argc<2)
-        return tp2(cin);
+        return solution(std::cin);
     else{
-        ifstream fichierPresences(argv[1]);
-        return tp2(fichierPresences);
+        std::ifstream fichierPresences(argv[1]);
+        return solution(fichierPresences);
     }
 }
